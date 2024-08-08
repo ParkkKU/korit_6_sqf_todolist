020b162e8bafe04f11762439e561548de1eebd82
parkkKU <pku02089@gmail.com>
2024-08-08 17:45:03 +0900

    240808

---
+++ todolist_back/src/main/java/com/study/todolist/dto/response/todo/RespAddTodoDto.java
@@ -0,0 +1,9 @@
+package com.study.todolist.dto.response.todo;
+
+public class RespAddTodoDto {
+    private String title;
+    private String content;
+    private String dateTime;
+    private int important;
+    private int busy;
+}
